ef10498e46ebb2db39653a681544742b01df53d5
NathanDaniel01 <nate@thedanielfamily.com>
2023-09-14 14:36:53 -0600

    Updated landing and fixed sub pages

---
+++ src/components/LetterPressBox.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "../App.css"
+import { BrowserRouter as Router, Route, Link, Routes, useParams } from "react-router-dom";
+
+function LetterPressBox(){
+    return(
+    <div class = "Box"> 
+        <h5>Letterpress:</h5>
+        <div class="imgFlex">
+            <div  class="contain L">
+                <img src="./Imgs/Raskog/Raskog1.jpg" alt=""class="image" />
+                <div class="middle">
+                    <Link class="titlesWhitetoRed" to={`/Raskog`}>Räskog</Link>
+                </div>
+            </div>
+            <div  class="contain R">
+                <img src="./Imgs/SixPack/Detail_9.jpg" alt=""class="image" />
+                <div class="middle">
+                    <Link class="titlesWhitetoRed" to={`/SixPack`}>6-pack</Link>
+                </div>
+            </div>   
+        </div>  
+        <div class="imgFlex">
+            <div  class="contain" style={{ margin: "5px 5px 120px auto"}}>
+                <img src="./Imgs/Raskog/Raskog1.jpg" alt=""class="image" />
+                <div class="middle">
+                    <Link class="titlesWhitetoRed" to={`/Raskog`}>Räskog</Link>
+                </div>
+            </div>
+            <div  class="contain" style={{ margin: "5px auto 120px 5px"}}>
+                <img src="./Imgs/SixPack/Detail_9.jpg" alt=""class="image" />
+                <div class="middle">
+                    <Link class="titlesWhitetoRed" to={`/SixPack`}>6-pack</Link>
+                </div>
+            </div>   
+        </div>     
+    </div>
+   );
+}
+export default LetterPressBox;
--- src/landing.js
+++ src/landing.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./App.css"
 import "./test.css"
-import  MyCanvas from "./testings"
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import ParallaxDemo from './components/paralax';
 import { ParallaxProvider} from 'react-scroll-parallax';
-import Dropdowns from './components/dropdown';
+import GraphicDesignBox from './components/GraphicDesignBox';
+import LetterPressBox from './components/LetterPressBox';
 import { BrowserRouter as Router, Route, Link, Routes, useParams } from "react-router-dom";
 import Footer from './components/Footer';
 import Canvas from './components/Canvas';
@@ -41,45 +39,19 @@ then when it is fully responsive the text will transition to static
 function Landing() {
     return (
         <div class = "mainBG"> 
+        <div></div>
         <a href="#top">
             <img src="./SVG/Home_Button.svg" alt=""class="nav sticky" style={{width:'50px'}}/>
             </a>
-            <h2 class = {'header1'}>Nathan</h2>
-            <h2 class = {'header2'}>Daniel</h2>
-            <p class = {'subHeader'}>Web Developer and Graphic Designer</p>
-            <Row>
-                <Col>
-                    <div class = {"lines1"}/>
-                    <div class = {"lines2"}/>
-                    <div class = {"lines3"}/>
-                </Col>
-
-                <Col>
-                
-                <a href="#section-1"> <div class = {"links"}>Graphic Design</div></a>
-                <a href="#section-1"> <div class = {"links"}>Letterpress</div></a>
-                <a href="#section-1"> <div class = {"links"}>Computer Science</div></a>
-                <a href="#section-4"> <div class = {"links"}>My Experience</div></a>
-                </Col>
-            </Row>
-         
-          
-        
+            <h2 class = {'header1 sticky'}>Nathan</h2>
+            <h2 class = {'header2 sticky'}>Daniel</h2>
             <ParallaxProvider>
-                <ParallaxDemo/>
-            </ParallaxProvider>
-            <div  id="section-1">
-                <Dropdowns/>
-                <div class = {'ContactPadding'}>    
-                <Row> 
-                        <Link to={`/Experience`} class = "dropTitles"id="section-4" >My Experience</Link>
-                   
-                </Row>
-                </div>
-            </div> 
-            <div class = "tree4">
-                    <img src="./SVG/trees_4.svg" alt="" />
-                </div>
+                    <ParallaxDemo/>
+                </ParallaxProvider>
+            <div class = 'externalspacertest'/>
+           <GraphicDesignBox/>
+           <div class='RedBG'/>
+           <LetterPressBox/>
             <Footer/>
         </div>
         );
